f4eda2942e0fab88bc2ab434ee90d99d10348b3c
Jeremy <jeremy2105@gmail.com>
2023-06-21 17:46:15 +0200

    integration page login non fonctionnel

---
+++ FrontEnd/javascript/login.js
@@ -0,0 +1,10 @@
+import { fetchWorksData, fetchCategoriesData } from './api.js';
+
+// {
+//     "email": "sophie.bluel@test.tld,
+//     "password": "S0phie"
+// }
+// {
+//     "userId": 1,
+//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
+// }
